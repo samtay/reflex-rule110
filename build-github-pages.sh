76b418d87565d2fa1dc78768f326d45d16721fb6
Sam Tay <sam.chong.tay@gmail.com>
2017-09-04 21:16:17 -0400

    Fix build script

--- build-github-pages.sh
+++ build-github-pages.sh
@@ -5,21 +5,27 @@ main() {
     errEcho "Building via ghcjs..."
     cabal configure --ghcjs
     cabal build
+
+    errEcho "Cloning gh-pages into tempdir..."
+    tmpdir=$(mktemp -d)
+    git clone --single-branch --branch gh-pages \
+        git@github.com:samtay/reflex-rule110.git \
+        $tmpdir
+
     errEcho "Pushing last dist/build to gh-pages branch..."
-    git checkout gh-pages
-    cp -R dist/build/rule110/rule110.jsexe/* .
+    cp -R dist/build/rule110/rule110.jsexe/* $tmpdir
+    cd $tmpdir
     git add .
     git commit -m 'Update ghcjs build output'
     git push
-    errEcho "Checking master branch back out..."
-    git checkout master
+
     errEcho "Done!"
 }
 
 errEcho() {
-    YELLOW='\033[1;36m'
+    CYAN='\033[1;36m'
     CLEAR='\033[0m'
-    >&2 echo -e "${YELLOW}""$@""${CLEAR}"
+    >&2 echo -e "${CYAN}""$@""${CLEAR}"
 }
 
 main "$@"
